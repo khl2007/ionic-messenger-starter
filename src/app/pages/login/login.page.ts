de2bcf4b7064212e63e52c38bf636efc10211893
khl2007 <khalilqandil@gmail.com>
2020-04-02 02:16:43 +0300

    addin login with email and password

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
 import { Router } from '@angular/router';
 import { MenuController, ToastController } from '@ionic/angular';
 
@@ -13,20 +16,54 @@ import * as firebase from 'firebase/app';
 })
 export class LoginPage implements OnInit {
 
+validations_form: FormGroup;
+  errorMessage: string = '';
+
+  validation_messages = {
+   'email': [
+     { type: 'required', message: 'Email is required.' },
+     { type: 'pattern', message: 'Please enter a valid email.' }
+   ],
+   'password': [
+     { type: 'required', message: 'Password is required.' },
+     { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+   ]
+ };
+
   constructor(
     private menuCtrl: MenuController, 
     private toastCtrl: ToastController,
+    private formBuilder: FormBuilder,
     private userService: UserService, 
     private router: Router
   ) {
   }
 
   ngOnInit() {
-    
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
   }
 
   ionViewWillEnter() {
     this.menuCtrl.enable(false);
+  }
+
+  tryLogin(value){
+    this.authService.doLogin(value)
+    .then(res => {
+      this.router.navigate(["/home"]);
+    }, err => {
+      this.errorMessage = err.message;
+      console.log(err)
+    })
   }
 
   loginWithFacebook() {

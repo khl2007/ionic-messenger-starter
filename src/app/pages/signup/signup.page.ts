c9bb7fd6cfb73ba49403c2005703fe83b2010834
khl2007 <khalilqandil@gmail.com>
2020-04-02 05:02:58 +0300

    Signup functions

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -10,24 +10,55 @@ import { Router } from '@angular/router';
 })
 export class SignupPage implements OnInit {
 
-  signupForm: FormGroup;
+  validations_form: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
 
-  constructor(private formBuilder: FormBuilder) {
-    this.signupForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+  validation_messages = {
+   'email': [
+     { type: 'required', message: 'Email is required.' },
+     { type: 'pattern', message: 'Enter a valid email.' }
+   ],
+   'password': [
+     { type: 'required', message: 'Password is required.' },
+     { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+   ]
+  };
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
     });
   }
 
-  ngOnInit() {
-    
+  tryRegister(value){
+    this.authService.doRegister(value)
+     .then(res => {
+       console.log(res);
+       this.errorMessage = "";
+       this.successMessage = "Your account has been created. Please log in.";
+     }, err => {
+       console.log(err);
+       this.errorMessage = err.message;
+       this.successMessage = "";
+     })
   }
 
-  onSignup() {
-    if (this.signupForm.valid) {
-      const credential = this.signupForm.getRawValue();
-      console.log(credential);
-    }
+  goLoginPage(){
+    this.router.navigate(["/login"]);
   }
 
 }
